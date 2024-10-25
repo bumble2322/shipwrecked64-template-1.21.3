f389d77c4b828a786a92bab25ff0c84ad49f38ce
bumble2322 <etaylevintal3@gmail.com>
2024-10-25 18:18:22 +0300

    lol

---
+++ src/main/java/name/modid/Shipwrecked64.java
@@ -0,0 +1,18 @@
+package name.modid;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Shipwrecked64 implements ModInitializer {
+	public static final String MOD_ID = "shipwrecked64";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+
+	}
+}
---
+++ src/main/java/name/modid/Shipwrecked64Client.java
@@ -0,0 +1,10 @@
+package name.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Shipwrecked64Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
